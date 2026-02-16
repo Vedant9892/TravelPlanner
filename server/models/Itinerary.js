d5e18637b735c98e0bc2134c5321cb5fdfd061c7
Vedant <vedantmahajan2017@gmail.com>
2026-02-16 23:01:04 +0530

    initial

---
+++ server/models/Itinerary.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const itinerarySchema = new mongoose.Schema(
+  {
+    tripId: { type: mongoose.Schema.Types.ObjectId, ref: 'Trip', required: true },
+    day: { type: Number, required: true },
+    activities: [{ title: String, description: String, time: String }],
+  },
+  { timestamps: true }
+)
+
+const Itinerary = mongoose.model('Itinerary', itinerarySchema)
+export default Itinerary
