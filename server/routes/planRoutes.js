3010606dcb31c4ae299b7f27cb26f20598ecfd9a
Vedant Mahajan <46640900+Vedant9892@users.noreply.github.com>
2026-02-17 01:56:13 +0530

    Delete server directory

--- server/routes/planRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createPlan, getPlan } from '../controllers/planController.js'
-
-const router = express.Router()
-
-router.post('/', createPlan)
-router.get('/:id', getPlan)
-
-export default router
